e2fb6dc9b7aa1da378d4d4dc984223ae73883757
umanrai58 <umanrai58@gmail.com>
2024-07-22 14:57:08 +0545

    added click event in Home component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-
       <div className="content">
         <Home />
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello, ninja', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello' + name, e.target);
+    }
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={ (e) => handleClickAgain('ninja', e) }>Click me again</button>
         </div>
     );
-}
+}   
  
 export default Home;
