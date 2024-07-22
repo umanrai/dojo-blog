5e1d02360b467b28f0693b2130f2aac4afc3e75e
umanrai58 <umanrai58@gmail.com>
2024-07-22 13:22:52 +0545

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1> { title } </h1>
+        <p>Liked { likes } times.</p>
+
+        <p>{ 10 }</p>
+        <p>{ "hello, Ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() *10 }</p>
+
+        <a href={ link } target="_blank">Google Site</a>
+      </div>
     </div>
   );
 }
