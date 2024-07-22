493e858f72f2ec635e250479dce160c1a60bb8c0
umanrai58 <umanrai58@gmail.com>
2024-07-22 13:35:00 +0545

    added multiple components
    Home & Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "https://www.google.com";
-
   return (
     <div className="App">
+      <Navbar />
+
       <div className="content">
-        <h1> { title } </h1>
-        <p>Liked { likes } times.</p>
-
-        <p>{ 10 }</p>
-        <p>{ "hello, Ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() *10 }</p>
-
-        <a href={ link } target="_blank">Google Site</a>
+        <Home />
       </div>
     </div>
   );
