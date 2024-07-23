ec770b2df65696f67692ab6c7185f147338445c3
umanrai58 <umanrai58@gmail.com>
2024-07-23 16:23:54 +0545

    added loading message & handling fetch error

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,35 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {          // res (response object)
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
+
                 return res.json();  // res.json=(this passes the json into a javascript object) then return it.
             })
             .then(data => {
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
             })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{ error }</div>}
+            { isPending && <div> Loading... </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />} {/* conditional templating "&& = logical and": its evaluates the left first and its true move to right */}
         </div>
     );
