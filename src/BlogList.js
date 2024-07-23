bf7df6d00b38492b31244d72d2c42388910594ef
umanrai58 <umanrai58@gmail.com>
2024-07-23 13:06:29 +0545

    added data for json server & fetching data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,6 @@
-const BlogList = ({blogs, title, handleDelete}) => {  // easy way to pass props
+const BlogList = ({blogs, title}) => {  // easy way to pass props
     // const blogs = props.blogs;
     // const title = props.title;
-    // const handleDelete = props.handleDelete;
 
     return ( 
         <div className="blog-list">
@@ -10,7 +9,6 @@ const BlogList = ({blogs, title, handleDelete}) => {  // easy way to pass props
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author} </p>
-                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,21 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id:1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id:2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id:3 }
-    ]);
-
-    const [name, setName] = useState('mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {          // res (response object)
+                return res.json();  // res.json=(this passes the json into a javascript object) then return it.
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>  
-            <button onClick={() => setName('luigi')}>Change name</button>
-            <p>{name}</p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />} {/* conditional templating "&& = logical and": its evaluates the left first and its true move to right */}
         </div>
     );
 }   
