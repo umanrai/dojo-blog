b881ea097b411c891e72d3e6bd5e32275e7c7acc
umanrai58 <umanrai58@gmail.com>
2024-07-24 18:57:50 +0545

    useNavigate hook and added deleting functionality

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const {data:blog, error, isPending} = useFetch(`http://localhost:8000/blogs/${id}`); // url concatenation: http://localhost:8000/blogs with id in template string.
+    const navigate = useNavigate(); // useNavigate hook is used for redirect
+
+    
+    const handleClick = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            console.log('blog deleted.')
+    
+            navigate('/'); // it navigate to home
+        })    
+    }
 
     return ( 
         <div className="blog-details">
@@ -14,6 +26,8 @@ const BlogDetails = () => {
                     <h2>{ blog.title}</h2>
                     <p>Written by { blog.author }</p>
                     <div>{blog.body}</div>
+                    {/* delete functionality */}
+                    <button onClick={handleClick}>delete</button> 
                 </article>
             )}
         </div>
--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate(); // useNavigate hook is used for redirect
 
     // submit event
     const handleSubmit = (e) => { 
@@ -20,6 +22,8 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added.')
             setIsPending(false);
+
+            navigate('/'); // it navigate to home
         })
     }
 
