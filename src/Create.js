5e6e84cd75cd6421da712e8ea1c014b74f0a71ef
umanrai58 <umanrai58@gmail.com>
2024-07-24 17:39:38 +0545

    added forms & submit events

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,44 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
-        <h2>Add a new Blog</h2>
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // it doesnt refresh the page
+        const blog = {title, body, author};
+        console.log(blog);
+    }
+
+    return (
+        <div className="create">
+            <h2>Add a new Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                type="text"
+                required
+                value={title} // above state value
+                onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                required
+                value={body} // above state value
+                onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                value={author} // above state value
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+        </div>
     );
 }
  
