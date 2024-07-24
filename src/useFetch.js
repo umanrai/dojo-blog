f3b411241b1133c3d9d307c58e802b04dd612f4b
umanrai58 <umanrai58@gmail.com>
2024-07-24 14:39:22 +0545

    added cleanup function

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController(); // to handle cleanup
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, {signal: abortCont.signal})
             .then(res => {          // res (response object)
                 if(!res.ok) {
                     throw Error('could not fetch the data for that resource');
@@ -21,10 +23,16 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
-                setIsPending(false);
-                setError(err.message);
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                } else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
             })
         }, 1000);
+
+        return () => abortCont.abort();
     }, [url]);
 
     return {data, isPending, error};
